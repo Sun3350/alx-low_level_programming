87bf911c23940e214e8e5f68306a8feb39919e37
ayomide <isaacayomide@gmail.com>
2022-03-11 03:48:34 -0800

    a C program that prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -33,15 +33,7 @@ int main(void)
 								printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 
 									printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+							printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 
-										printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-
-											printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-
-
-
-												return (0);
-
-}
-
-© 2022 GitHub, Inc.
+				printf("Size of a float: %lu byte(s)\n", sizeof(afloat))
+												return (0)}
